00b2183d60ff11a026c5e6b85b90900008910d30
morn-0 <90960002+morn-0@users.noreply.github.com>
2024-04-02 17:25:45 +0800

    Remove nightly toolchain requirement

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,6 @@
 //!
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(incomplete_features)]
-#![feature(return_position_impl_trait_in_trait)]
-#![feature(async_fn_in_trait)]
 #![forbid(unsafe_code, future_incompatible)]
 #![deny(missing_debug_implementations, nonstandard_style)]
 #![warn(missing_docs)]
